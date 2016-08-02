dea7867e4d5d069ce577bae40e1da0dde9059992
Alexis Montagne <alexis.montagne@gmail.com>
2016-08-02 15:54:11 -0400

    Update the packages distributed

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name='upfluence-utils',
-      version='0.0.1',
+      version='0.1.0',
       description='set of utils used at Upfluence',
       author='Alexis Montagne',
       author_email='alexis.montagne@upfluence.co',
       url='https://github.com/upfluence/pyutils',
-      packages=['upfluence'],
+      packages=['upfluence.error_logger', 'upfluence.log', 'upfluence.tracing',
+                'upfluence.thrift'],
       install_requires=['thrift', 'tornado'])
