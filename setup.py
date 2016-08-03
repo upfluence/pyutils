e866090f5043a7779ddd1a8515e58031d33a7408
Alexis Montagne <alexis.montagne@gmail.com>
2016-08-03 14:42:35 -0400

    Also import the upfluence package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='upfluence-utils',
-      version='0.1.0',
+      version='0.1.1',
       description='set of utils used at Upfluence',
       author='Alexis Montagne',
       author_email='alexis.montagne@upfluence.co',
       url='https://github.com/upfluence/pyutils',
       packages=['upfluence.error_logger', 'upfluence.log', 'upfluence.tracing',
-                'upfluence.thrift'],
+                'upfluence.thrift', 'upfluence'],
       install_requires=['thrift', 'tornado'])
